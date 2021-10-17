b6856fe5b37b3935c460273795d94c7be68a282c
Tomas <tonken.ek@gmail.com>
2021-10-17 08:54:33 +0100

    refactor sign second

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import './SignInPage.css'
-import { Button } from '../../components/button/Button'; 
+import { Button } from '../../components/button/Button';
 import RoutingPath from "../../routes/RoutingPath";
 
 export const SignInPage = () => {
@@ -11,26 +11,26 @@ export const SignInPage = () => {
     return (
         <div className="signIn--wrapper">
             <div className="signIn--container">
-            <div className="content--wrapper">
-                <div className="promptText--wrapper">
-                    {/* /* LÖS UTAN BR*/ }
-                    <h1>Sign in to <br /> devFlow.</h1>
-                    <p className="p-large">Don't have an account?</p>
-                    <button onClick={() => history.push(RoutingPath.signUpPage)}>Create Account</button>
+                <div className="content--wrapper">
+                    <div className="promptText--wrapper">
+                        {/* /* LÖS UTAN BR*/}
+                        <h1 className="signIn--title">Sign in to devFlow.</h1>
+                        <p className="p-large">Don't have an account?</p>
+                        <p className="p-normal p--link" onClick={() => history.push(RoutingPath.signUpPage)}>Create Account</p>
+                    </div>
+                    {/* använd iaf state form får du om du vill*/}
+                    <form className="form--inputs">
+                        <input type="email" className="inputfield" name="email" placeholder="email..." />
+                        <input type="password" className="inputfield" name="password" placeholder="password.." />
+                    </form>
+                    <div className="signIn--btn-wrap">
+                        <a href="#">Forgot password?</a>
+                        <Button
+                            customCssClass="btn-primary signIn--button"
+                            text="Sign In"
+                        />
+                    </div>
                 </div>
-                {/* använd iaf state form får du om du vill*/ }
-                <div className="signIn--inputs">
-                    <input type="email" className="inputfield" name="email" placeholder="email..." />
-                    <input type="password" className="inputfield" name="password" placeholder="password.." />
-                </div>
-                <div className="signIn--btn-wrap">
-                <a href="#">Forgot password?</a>
-                <Button
-                customCssClass="btn-primary signIn--button"
-                text="Sign In"
-              />
-              </div>
-              </div>
             </div>
         </div>
     )
