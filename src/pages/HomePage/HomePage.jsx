3395f94f4623761b29ba8d5201ec5acf85445629
Kaj Berg <kb223aw@student.lnu.se>
2021-10-18 11:48:28 +0200

    Main page done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './HomePage.css'
+import React from 'react';
+import './HomePage.css';
+import startImage from '../../utils/images/startimage.svg';
+import { Button } from '../../components/button/Button';
 
 export const HomePage = () => {
     return (
-    <div>
-        <h1>HomePage</h1>
+<main className="main-container">
+    <div className="main-wrapper">
+        <h1>The Productivity tool for developer students.</h1>
+        <Button customCssClass="btn-primary signIn--button" text="Pomodoro" />
     </div>
+    
+    <img src={startImage} alt="Person reading a box while sitting on a box" className="mainImage">
+    </img>
+
+</main>
     )
 }
