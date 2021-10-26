3c1a2f0dbb7a3db50e5a0f87f5fe826a9a400606
Mattis <m.erkensten@icloud.com>
2021-10-26 14:21:58 +0200

    Dashboard kod grund tillagd

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './DashboardPage.css'
+import "../HomePage/HomePage.css";
+import startImage from "../../utils/images/startimage.svg";
+import { Button } from "../../components/button/Button";
+import RoutingPath from "../../routes/RoutingPath";
+
 
 export const DashboardPage = () => {
-    return (
-        <>
-            <h1>Signed In</h1>
-        </>
-    )
-}
+  return (
+    <main className="main-container">
+      <div className="main-wrapper">
+        <div className="main-content">
+            <h1>The Productivity tool for developer students.</h1>
+            <Button className="btn-home" customCssClass="btn-primary signIn--button" text="Pomodoro"
+            targetPage={RoutingPath.pomodoroPage} />
+            <Button className="btn-home" customCssClass="btn-primary signIn--button" text="To Do"
+            targetPage={RoutingPath.todoPage} />
+        </div>
+        <img
+          src={startImage}
+          alt="Person reading a box while sitting on a box"
+          className="mainImage"
+        ></img>
+      </div>
+    </main>
+  );
+};
